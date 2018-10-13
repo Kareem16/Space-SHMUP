47a37aa48c2fb0268309955b981daf0d1f9105e3
Kareem16 <35926953+Kareem16@users.noreply.github.com>
2018-10-12 20:23:43 -0400

    Done In Class 10-12
    Continue @ pg 678 part a.

--- Assets/__Scripts/Shield.cs
+++ Assets/__Scripts/Shield.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Shield : MonoBehaviour {
+
     [Header("Set in Inspector")]
     public float rotationsPerSecond = 0.1f;
+
     [Header("Set Dynamically")]
     public int levelShown = 0;
+
     // This non-public variable will not appear in the Inspector
     Material mat; // a
 
@@ -19,9 +22,8 @@ public class Shield : MonoBehaviour {
 	void Update () {
         // Read the current shield level from the Hero Singleton
         int currLevel = Mathf.FloorToInt(Hero.S.shieldLevel); // c
-                                                              // If this is different from levelShown...
-        if (levelShown != currLevel)
-        {
+        // If this is different from levelShown...
+        if (levelShown != currLevel) {
             levelShown = currLevel;
             // Adjust the texture offset to show different shield level
             mat.mainTextureOffset = new Vector2(0.2f * levelShown, 0); // d
