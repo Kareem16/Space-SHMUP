acda43789e1e03ead59c60b9df627ad359cfa065
Kareem16 <35926953+Kareem16@users.noreply.github.com>
2018-10-18 22:26:20 -0400

    Done by end of class 10/18/18
    @ pg 690 number 4.

--- Assets/__Scripts/BoundsCheck.cs
+++ Assets/__Scripts/BoundsCheck.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class BoundsCheck : MonoBehaviour {
     [Header("Set in Inspector")]
     public float radius = 1f;
+    public bool keepOnScreen = true;
 
     [Header("Set Dynamically")]
+    public bool isOnScreen = true;
     public float camWidth;
     public float camHeight;
+    [HideInInspector]
+    public bool     offRight, offLeft, offUp, offDown;
 
     void Awake() {
         camHeight = Camera.main.orthographicSize;
@@ -17,22 +21,33 @@ public class BoundsCheck : MonoBehaviour {
 
     void LateUpdate() {
         Vector3 pos = transform.position;
+        isOnScreen = true;
+        offRight = offLeft = offUp = offDown = false;
 
         if (pos.x > camWidth - radius) {
             pos.x = camWidth - radius;
+            offRight = true;
         }
 
         if (pos.x < -camWidth + radius) {
             pos.x = -camWidth + radius;
+            offLeft = true;
         }
         if (pos.y > camHeight - radius) {
-            pos.y = camHeight - radius; 
+            pos.y = camHeight - radius;
+            offUp = true;
         }
         if (pos.y < -camHeight + radius) {
             pos.y = -camHeight + radius;
+            offDown = true;
         }
 
-        transform.position = pos;
+        isOnScreen = !(offRight || offLeft || offDown);
+        if (keepOnScreen && !isOnScreen) {
+            transform.position = pos;
+            isOnScreen = true;
+            offRight = offLeft = offUp = offDown = false;
+        }
     }
 
     //Draw the bounds in the Scene pane using OnDrawGizmos()
